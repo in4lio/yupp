e30a48ed6dccef4c7f82e5636e6b6491ebd60384
Vitaly Kravtsov <in4lio@gmail.com>
2015-02-23 16:32:08 +0300

    0.8b1
    (1) saving data for browsing the source code of files generated by preprocessor into "json" file
    (2) Sublime Text plug-in for browsing the source code
    (3) PP_BROWSE (--pp-browse) command line parameter has been added

--- sublime_text/Data/Packages/yupp/readonly.py
+++
@@ -1,9 +0,0 @@
-import os
-import sublime
-import sublime_plugin
-
-class readonly( sublime_plugin.EventListener ):
-    def on_activated( self, view ):
-        fn = view.file_name()
-        if fn:
-            view.set_read_only( not os.access( fn, os.W_OK ))
