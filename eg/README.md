4fc257e365339f5df67f3d3c469281961dad9e9b
in4lio <in4lio@gmail.com>
2016-09-05 14:12:18 +0300

    added example of coroutines on Python

--- eg/README.md
+++ eg/README.md
@@ -1,1 +1,3 @@
 [A glance at the yupp](../../master/doc/glance.md)
+[Coroutines on C](./ulam.yu-c)
+[Coroutines on Python](./coro.yu-py)
---
+++ eg/coro.py
@@ -0,0 +1,44 @@
+
+CO_READY = 0
+CO_WAIT  = 1
+CO_YIELD = 2
+CO_END   = 3
+CO_SKIP  = 4
+
+c = False
+
+def coro_A():
+    while True:
+        print 'A'
+        yield CO_YIELD
+
+def coro_B():
+    global c
+
+    for i in xrange( 5 ):
+        print 'B'
+        if i & 1:
+            c = True
+        yield CO_YIELD
+
+    while True: yield CO_END
+
+def coro_C():
+    global c
+
+    while True:
+        while not ( c ): yield CO_WAIT
+        print 'C'
+        c = False
+
+if __name__ == '__main__':
+    A = coro_A()
+    B = coro_B()
+    C = coro_C()
+
+    while True:
+        A.next()
+        if not ((  B.next() ) < CO_END ):
+            break
+        C.next()
+        print
